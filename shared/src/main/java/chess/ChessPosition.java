2b2df29d656a36bc04b213b2927cf637ed0a3d9c
Corban Thompson <corbant25@gmail.com>
2026-01-02 15:28:10 -0800

    feat: implement ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,22 +8,27 @@ package chess;
  */
 public class ChessPosition {
 
+    private int row;
+    private int col;
+
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
      * @return which row this position is in
-     * 1 codes for the bottom row
+     *         1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return this.row;
     }
 
     /**
      * @return which column this position is in
-     * 1 codes for the left row
+     *         1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return this.col;
     }
 }
