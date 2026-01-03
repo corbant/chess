f86eef056a2d70a2b6ef9542e8d14ec263d17255
Corban Thompson <corbant25@gmail.com>
2026-01-02 17:33:00 -0800

    feat: implement ChessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,8 +10,12 @@ package chess;
  */
 public class ChessBoard {
 
+    private static final int BOARD_ROWS = 8;
+    private static final int BOARD_COLS = 8;
+    private ChessPiece[][] board;
+
     public ChessBoard() {
-        
+        this.board = new ChessPiece[ChessBoard.BOARD_ROWS][ChessBoard.BOARD_COLS];
     }
 
     /**
@@ -19,7 +25,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -27,10 +33,32 @@ public class ChessBoard {
      *
      * @param position The position to get the piece from
      * @return Either the piece at the position, or null if no piece is at that
-     * position
+     *         position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow() - 1][position.getColumn() - 1];
+    }
+
+    /**
+     * Removes a chess piece from the given position
+     * 
+     * @param position The position to remove the piece from
+     */
+    public void removePiece(ChessPosition position) {
+        board[position.getRow() - 1][position.getColumn() - 1] = null;
+    }
+
+    /**
+     * Moves a chess piece from one position to another
+     * 
+     * @param startPosition position of the piece to move
+     * @param endPosition   location to move the piece to
+     */
+    public void movePiece(ChessPosition startPosition, ChessPosition endPosition) {
+        ChessPiece piece = this.getPiece(startPosition);
+        // pick up the piece and place it in the new location
+        this.removePiece(endPosition);
+        this.addPiece(endPosition, piece);
     }
 
     /**
@@ -38,6 +66,62 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        // create a new board
+        this.board = new ChessPiece[ChessBoard.BOARD_ROWS][ChessBoard.BOARD_COLS];
+
+        // add pieces
+        for (ChessGame.TeamColor teamColor : ChessGame.TeamColor.values()) {
+            boolean isWhite = teamColor == ChessGame.TeamColor.WHITE;
+            int row = isWhite ? 0 : ChessBoard.BOARD_ROWS - 1;
+
+            // rooks
+            board[row][0] = new ChessPiece(teamColor, ChessPiece.PieceType.ROOK);
+            board[row][ChessBoard.BOARD_COLS - 1] = new ChessPiece(teamColor, ChessPiece.PieceType.ROOK);
+
+            // knights
+            board[row][1] = new ChessPiece(teamColor, ChessPiece.PieceType.KNIGHT);
+            board[row][ChessBoard.BOARD_COLS - 1 - 1] = new ChessPiece(teamColor, ChessPiece.PieceType.KNIGHT);
+
+            // bishops
+            board[row][2] = new ChessPiece(teamColor, ChessPiece.PieceType.BISHOP);
+            board[row][ChessBoard.BOARD_COLS - 1 - 2] = new ChessPiece(teamColor, ChessPiece.PieceType.BISHOP);
+
+            // royalty
+            board[row][3] = new ChessPiece(teamColor, ChessPiece.PieceType.QUEEN);
+            board[row][ChessBoard.BOARD_COLS - 1 - 3] = new ChessPiece(teamColor, ChessPiece.PieceType.KING);
+
+            // pawns
+            row += isWhite ? 1 : -1;
+            for (int i = 0; i < ChessBoard.BOARD_COLS; i++) {
+                board[row][i] = new ChessPiece(teamColor, ChessPiece.PieceType.PAWN);
+            }
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.deepHashCode(board);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ChessBoard other = (ChessBoard) obj;
+        if (!Arrays.deepEquals(board, other.board))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard [board=" + Arrays.toString(board) + "]";
     }
 }
